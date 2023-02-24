f8175aa7298fe9b150d71bb93a3f10ee092c5995
Timo Dankert <timo.dankert@posteo.net>
2023-02-24 12:34:40 +0100

    Added a really simple converter
    Automapper seems kind of complicated in that case, when it comes to creating hop lists etc.

--- Brauser.Common/Recipe/Recipe.cs
+++ Brauser.Common/Recipe/Recipe.cs
@@ -2,41 +2,41 @@ namespace Brauser.Common.Recipe;
 
 public record Recipe
 {
-    public string Name { get; set; }
-    public DateTimeOffset Date { get; set; }
-    public string Type { get; set; }
+    public string? Name { get; set; }
+    public DateTimeOffset? Date { get; set; }
+    public string? Type { get; set; }
 
     public int? BatchSize { get; set; } // Ausschlagwürze
     public int? BrewhouseYield { get; set; } // Sudhausausbeute
     public int? OriginalGravity { get; set; } // Stammwürze
     public int? Bitterness { get; set; } // Bittere
 
-    public string color { get; set; }
-    public string alcohol { get; set; }
-    public string ShortDescription { get; set; }
+    public string? Color { get; set; }
+    public double? Alcohol { get; set; }
+    public string? ShortDescription { get; set; }
 
-    public int MashWater { get; set; } // Hauptguss
+    public int? MashWater { get; set; } // Hauptguss
 
-    public List<Malt> Malts { get; set; }
+    public List<Malt>? Malts { get; set; }
 
-    public double MashingTemperature { get; set; } // InfusionEinmaischTemperatur
+    public double? MashingTemperature { get; set; } // InfusionEinmaischTemperatur
 
-    public List<Infusion> Infusions { get; set; }
+    public List<Infusion>? Infusions { get; set; }
 
-    public double MashoutTemperature { get; set; } // Abmaischtemperatur
+    public double? MashoutTemperature { get; set; } // Abmaischtemperatur
 
-    public int SpargeWater { get; set; } // Nachguss
+    public int? SpargeWater { get; set; } // Nachguss
 
-    public string WortBoilingTime { get; set; } // Kochzeitwürze
+    public string? WortBoilingTime { get; set; } // Kochzeitwürze
 
-    public string HopVWH_1_Variety { get; set; } // Sorte
-    public double HopVWH_1_Quantity { get; set; } // Menge
-    public int HopVW_1_Alpha { get; set; }
+    public string? HopVWH_1_Variety { get; set; } // Sorte
+    public double? HopVWH_1_Quantity { get; set; } // Menge
+    public int? HopVW_1_Alpha { get; set; }
 
-    public List<Hop> Hops { get; set; }
-    public string Yeast { get; set; }
-    public double FermentationTemperature { get; set; } // Gaertemperatur
-    public string FinalDegreeOfFermentation { get; set; } // Endvergaerungsgrad
-    public string Carbonation { get; set; }
-    public string AuthorNotes { get; set; }
+    public List<Hop>? Hops { get; set; }
+    public string? Yeast { get; set; }
+    public double? FermentationTemperature { get; set; } // Gaertemperatur
+    public string? FinalDegreeOfFermentation { get; set; } // Endvergaerungsgrad
+    public string? Carbonation { get; set; }
+    public string? AuthorNotes { get; set; }
 }
