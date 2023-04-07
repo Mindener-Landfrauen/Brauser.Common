a5250401cbe52124deea663808153a0d1c6c4cae
Timo Dankert <timo.dankert@posteo.net>
2023-04-07 18:24:31 +0200

    reafactor: Added order

--- Brauser.Common/Recipe/DryHop.cs
+++ Brauser.Common/Recipe/DryHop.cs
@@ -3,6 +3,7 @@ namespace Brauser.Common.Recipe;
 public class DryHop
 {
     public Guid Id { get; set; }
+    public int? Order { get; set; }
     public string? Variety { get; set; }
     public string? Quantity { get; set; }
 }
