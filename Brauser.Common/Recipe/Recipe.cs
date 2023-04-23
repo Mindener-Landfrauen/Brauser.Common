ac779a57b98d3494b0b0073eabf608f01520b1a3
Lars Richter <github@n-develop.com>
2023-04-23 16:31:09 +0200

    feat: create new objects for general information, brewing process and fermentation

--- Brauser.Common/Recipe/Recipe.cs
+++ Brauser.Common/Recipe/Recipe.cs
@@ -2,6 +2,13 @@ namespace Brauser.Common.Recipe;
 
 public record Recipe
 {
+    public Recipe()
+    {
+        GeneralInformation = new GeneralInformation();
+        BrewingProcess = new BrewingProcess();
+        FermentationAndMaturing = new FermentationAndMaturing();
+    }
+    
     public Guid Id { get; set; }
 
     public GeneralInformation GeneralInformation { get; set; }
